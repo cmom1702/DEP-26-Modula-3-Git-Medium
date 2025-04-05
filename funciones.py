c6cb608655de700a35191a8600286631ab33ceb1
Mauricio Oyarzo <cmom1702@gmail.com>
2025-04-05 09:26:01 -0300

    Se agrega funcion 2 a funciones.py

--- funciones.py
+++ funciones.py
@@ -1,3 +1,7 @@
 def miFuncion1():    # Esta es la función 1
     print("Esta es la función 1")
     return 1
+
+def miFuncion2():    # Esta es la función 2
+    print("Esta es la función 2")
+    return 1
